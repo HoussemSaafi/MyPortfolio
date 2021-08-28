a766669ef40ffe33b3ab627499c3d8b40f8ce014
Houssem Saafi <69660781+HoussemSaafi@users.noreply.github.com>
2021-08-28 16:52:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
+const app = express();
 app.use(requireHTTPS);
-app.use(express.static(’./dist/<name-on-package.json>’));
+app.use(express.static(’./dist/my-first-app’));
 app.get('/*', function(req, res) {
-  res.sendFile(’index.html’, {root: 'dist/<name-on-package.json>/’}
+  res.sendFile(’index.html’, {root: 'dist/my-first-app/’}
 );
 });
 app.listen(process.env.PORT || 8080);
